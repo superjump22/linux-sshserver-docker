70488de6a2f44d24599aa91b9f47bd402f1c6964
Super Jump <superjump.void@gmail.com>
2024-04-17 22:48:02 +0800

    fix centos

--- dockerfiles/centos/install-ssh.sh
+++ dockerfiles/centos/install-ssh.sh
@@ -6,25 +6,22 @@ mkdir -p /var/run/sshd
 case "$DIST_VERSION" in
 7)
 	sed -e 's|^mirrorlist=|#mirrorlist=|g' \
-		-e 's|^#baseurl=https://mirror.centos.org/centos|baseurl=https://mirrors.tencentyun.com/centos|g' \
+		-e 's|^#baseurl=http://mirror.centos.org/centos|baseurl=https://mirrors.tencentyun.com/centos|g' \
 		-i.bak \
 		/etc/yum.repos.d/CentOS-*.repo
 	;;
 stream8)
 	sed -e 's|^mirrorlist=|#mirrorlist=|g' \
-		-e 's|^#baseurl=https://mirror.centos.org/$contentdir|baseurl=https://mirrors.tencentyun.com/centos|g' \
+		-e 's|^#baseurl=http://mirror.centos.org/$contentdir|baseurl=https://mirrors.tencentyun.com/centos|g' \
 		-i.bak \
-		/etc/yum.repos.d/CentOS-Stream-AppStream.repo \
-		/etc/yum.repos.d/CentOS-Stream-BaseOS.repo \
-		/etc/yum.repos.d/CentOS-Stream-Extras.repo \
-		/etc/yum.repos.d/CentOS-Stream-PowerTools.repo
+		/etc/yum.repos.d/CentOS-*.repo
 	;;
 stream9)
 	cat <<EOF >/etc/yum.repos.d/centos.repo
 [baseos]
 name=CentOS Stream $releasever - BaseOS
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/BaseOS/$basearch/os
-# metalink=https://mirrors.centos.org/metalink?repo=centos-baseos-$stream&arch=$basearch&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-baseos-$stream&arch=$basearch&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -35,7 +32,7 @@ enabled=1
 [baseos-debuginfo]
 name=CentOS Stream $releasever - BaseOS - Debug
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/BaseOS/$basearch/debug/tree/
-# metalink=https://mirrors.centos.org/metalink?repo=centos-baseos-debug-$stream&arch=$basearch&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-baseos-debug-$stream&arch=$basearch&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -45,7 +42,7 @@ enabled=0
 [baseos-source]
 name=CentOS Stream $releasever - BaseOS - Source
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/BaseOS/source/tree/
-# metalink=https://mirrors.centos.org/metalink?repo=centos-baseos-source-$stream&arch=source&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-baseos-source-$stream&arch=source&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -55,7 +52,7 @@ enabled=0
 [appstream]
 name=CentOS Stream $releasever - AppStream
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/AppStream/$basearch/os
-# metalink=https://mirrors.centos.org/metalink?repo=centos-appstream-$stream&arch=$basearch&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-appstream-$stream&arch=$basearch&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -66,7 +63,7 @@ enabled=1
 [appstream-debuginfo]
 name=CentOS Stream $releasever - AppStream - Debug
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/AppStream/$basearch/debug/tree/
-# metalink=https://mirrors.centos.org/metalink?repo=centos-appstream-debug-$stream&arch=$basearch&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-appstream-debug-$stream&arch=$basearch&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -76,7 +73,7 @@ enabled=0
 [appstream-source]
 name=CentOS Stream $releasever - AppStream - Source
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/AppStream/source/tree/
-# metalink=https://mirrors.centos.org/metalink?repo=centos-appstream-source-$stream&arch=source&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-appstream-source-$stream&arch=source&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -86,7 +83,7 @@ enabled=0
 [crb]
 name=CentOS Stream $releasever - CRB
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/CRB/$basearch/os
-# metalink=https://mirrors.centos.org/metalink?repo=centos-crb-$stream&arch=$basearch&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-crb-$stream&arch=$basearch&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -97,7 +94,7 @@ enabled=1
 [crb-debuginfo]
 name=CentOS Stream $releasever - CRB - Debug
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/CRB/$basearch/debug/tree/
-# metalink=https://mirrors.centos.org/metalink?repo=centos-crb-debug-$stream&arch=$basearch&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-crb-debug-$stream&arch=$basearch&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -107,7 +104,7 @@ enabled=0
 [crb-source]
 name=CentOS Stream $releasever - CRB - Source
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/CRB/source/tree/
-# metalink=https://mirrors.centos.org/metalink?repo=centos-crb-source-$stream&arch=source&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-crb-source-$stream&arch=source&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -118,7 +115,7 @@ EOF
 [highavailability]
 name=CentOS Stream $releasever - HighAvailability
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/HighAvailability/$basearch/os
-# metalink=https://mirrors.centos.org/metalink?repo=centos-highavailability-$stream&arch=$basearch&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-highavailability-$stream&arch=$basearch&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -129,7 +126,7 @@ enabled=0
 [highavailability-debuginfo]
 name=CentOS Stream $releasever - HighAvailability - Debug
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/HighAvailability/$basearch/debug/tree/
-# metalink=https://mirrors.centos.org/metalink?repo=centos-highavailability-debug-$stream&arch=$basearch&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-highavailability-debug-$stream&arch=$basearch&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -139,7 +136,7 @@ enabled=0
 [highavailability-source]
 name=CentOS Stream $releasever - HighAvailability - Source
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/HighAvailability/source/tree/
-# metalink=https://mirrors.centos.org/metalink?repo=centos-highavailability-source-$stream&arch=source&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-highavailability-source-$stream&arch=source&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -149,7 +146,7 @@ enabled=0
 [nfv]
 name=CentOS Stream $releasever - NFV
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/NFV/$basearch/os
-# metalink=https://mirrors.centos.org/metalink?repo=centos-nfv-$stream&arch=$basearch&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-nfv-$stream&arch=$basearch&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -160,7 +157,7 @@ enabled=0
 [nfv-debuginfo]
 name=CentOS Stream $releasever - NFV - Debug
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/NFV/$basearch/debug/tree/
-# metalink=https://mirrors.centos.org/metalink?repo=centos-nfv-debug-$stream&arch=$basearch&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-nfv-debug-$stream&arch=$basearch&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -170,7 +167,7 @@ enabled=0
 [nfv-source]
 name=CentOS Stream $releasever - NFV - Source
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/NFV/source/tree/
-# metalink=https://mirrors.centos.org/metalink?repo=centos-nfv-source-$stream&arch=source&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-nfv-source-$stream&arch=source&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -180,7 +177,7 @@ enabled=0
 [rt]
 name=CentOS Stream $releasever - RT
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/RT/$basearch/os
-# metalink=https://mirrors.centos.org/metalink?repo=centos-rt-$stream&arch=$basearch&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-rt-$stream&arch=$basearch&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -191,7 +188,7 @@ enabled=0
 [rt-debuginfo]
 name=CentOS Stream $releasever - RT - Debug
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/RT/$basearch/debug/tree/
-# metalink=https://mirrors.centos.org/metalink?repo=centos-rt-debug-$stream&arch=$basearch&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-rt-debug-$stream&arch=$basearch&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -201,7 +198,7 @@ enabled=0
 [rt-source]
 name=CentOS Stream $releasever - RT - Source
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/RT/source/tree/
-# metalink=https://mirrors.centos.org/metalink?repo=centos-rt-source-$stream&arch=source&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-rt-source-$stream&arch=source&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -211,7 +208,7 @@ enabled=0
 [resilientstorage]
 name=CentOS Stream $releasever - ResilientStorage
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/ResilientStorage/$basearch/os
-# metalink=https://mirrors.centos.org/metalink?repo=centos-resilientstorage-$stream&arch=$basearch&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-resilientstorage-$stream&arch=$basearch&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -222,7 +219,7 @@ enabled=0
 [resilientstorage-debuginfo]
 name=CentOS Stream $releasever - ResilientStorage - Debug
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/ResilientStorage/$basearch/debug/tree/
-# metalink=https://mirrors.centos.org/metalink?repo=centos-resilientstorage-debug-$stream&arch=$basearch&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-resilientstorage-debug-$stream&arch=$basearch&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -232,7 +229,7 @@ enabled=0
 [resilientstorage-source]
 name=CentOS Stream $releasever - ResilientStorage - Source
 baseurl=https://mirrors.tencentyun.com/centos-stream/$releasever-stream/ResilientStorage/source/tree/
-# metalink=https://mirrors.centos.org/metalink?repo=centos-resilientstorage-source-$stream&arch=source&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-resilientstorage-source-$stream&arch=source&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 gpgcheck=1
 repo_gpgcheck=0
@@ -242,7 +239,7 @@ enabled=0
 [extras-common]
 name=CentOS Stream $releasever - Extras packages
 baseurl=https://mirrors.tencentyun.com/centos-stream/SIGs/$releasever-stream/extras/$basearch/extras-common
-# metalink=https://mirrors.centos.org/metalink?repo=centos-extras-sig-extras-common-$stream&arch=$basearch&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-extras-sig-extras-common-$stream&arch=$basearch&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Extras-SHA512
 gpgcheck=1
 repo_gpgcheck=0
@@ -253,7 +250,7 @@ enabled=1
 [extras-common-source]
 name=CentOS Stream $releasever - Extras packages - Source
 baseurl=https://mirrors.tencentyun.com/centos-stream/SIGs/$releasever-stream/extras/source/extras-common
-# metalink=https://mirrors.centos.org/metalink?repo=centos-extras-sig-extras-common-source-$stream&arch=source&protocol=https,https
+# metalink=https://mirrors.centos.org/metalink?repo=centos-extras-sig-extras-common-source-$stream&arch=source&protocol=https,http
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Extras-SHA512
 gpgcheck=1
 repo_gpgcheck=0
--- dockerfiles/fedora/install-ssh.sh
+++ dockerfiles/fedora/install-ssh.sh
@@ -6,7 +6,7 @@ mkdir -p /var/run/sshd
 case "$DIST_VERSION" in
 38)
 	sed -e 's|^metalink=|#metalink=|g' \
-		-e 's|^#baseurl=https://download.example/pub/fedora/linux|baseurl=https://mirrors.tencentyun.com/fedora|g' \
+		-e 's|^#baseurl=http://download.example/pub/fedora/linux|baseurl=https://mirrors.tencentyun.com/fedora|g' \
 		-i.bak \
 		/etc/yum.repos.d/fedora.repo \
 		/etc/yum.repos.d/fedora-modular.repo \
@@ -15,10 +15,10 @@ case "$DIST_VERSION" in
 	;;
 39 | 40 | 41)
 	sed -e 's|^metalink=|#metalink=|g' \
-		-e 's|^#baseurl=https://download.example/pub/fedora/linux|baseurl=https://mirrors.tencentyun.com/fedora|g' \
+		-e 's|^#baseurl=http://download.example/pub/fedora/linux|baseurl=https://mirrors.tencentyun.com/fedora|g' \
 		-i.bak \
 		/etc/yum.repos.d/fedora.repo \
-		/etc/yum.repos.d/fedora-updates.repo \
+		/etc/yum.repos.d/fedora-updates.repo
 	;;
 *)
 	echo "Unsupported version: $DIST_VERSION"
