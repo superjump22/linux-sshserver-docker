5c29d6f8867195725aabce3f140f4261a340371a
Super Jump <superjump.void@gmail.com>
2024-04-17 22:18:28 +0800

    tencent

--- dockerfiles/debian/install-ssh.sh
+++ dockerfiles/debian/install-ssh.sh
@@ -6,38 +6,38 @@ mkdir -p /var/run/sshd
 case "$DIST_VERSION" in
 10)
 	cat <<EOF >/etc/apt/sources.list
-deb https://mirrors.tencentyun.com/debian/ bullseye main contrib non-free
-# deb-src https://mirrors.tencentyun.com/debian/ bullseye main contrib non-free
-deb https://mirrors.tencentyun.com/debian/ bullseye-updates main contrib non-free
-# deb-src https://mirrors.tencentyun.com/debian/ bullseye-updates main contrib non-free
-deb https://mirrors.tencentyun.com/debian/ bullseye-backports main contrib non-free
-# deb-src https://mirrors.tencentyun.com/debian/ bullseye-backports main contrib non-free
-deb https://mirrors.tencentyun.com/debian-security bullseye-security main contrib non-free
-# deb-src https://mirrors.tencentyun.com/debian-security bullseye-security main contrib non-free
+deb http://mirrors.tencentyun.com/debian/ bullseye main contrib non-free
+# deb-src http://mirrors.tencentyun.com/debian/ bullseye main contrib non-free
+deb http://mirrors.tencentyun.com/debian/ bullseye-updates main contrib non-free
+# deb-src http://mirrors.tencentyun.com/debian/ bullseye-updates main contrib non-free
+deb http://mirrors.tencentyun.com/debian/ bullseye-backports main contrib non-free
+# deb-src http://mirrors.tencentyun.com/debian/ bullseye-backports main contrib non-free
+deb http://mirrors.tencentyun.com/debian-security bullseye-security main contrib non-free
+# deb-src http://mirrors.tencentyun.com/debian-security bullseye-security main contrib non-free
 EOF
 	;;
 11)
 	cat <<EOF >/etc/apt/sources.list
-deb https://mirrors.tencentyun.com/debian/ bullseye main contrib non-free
-# deb-src https://mirrors.tencentyun.com/debian/ bullseye main contrib non-free
-deb https://mirrors.tencentyun.com/debian/ bullseye-updates main contrib non-free
-# deb-src https://mirrors.tencentyun.com/debian/ bullseye-updates main contrib non-free
-deb https://mirrors.tencentyun.com/debian/ bullseye-backports main contrib non-free
-# deb-src https://mirrors.tencentyun.com/debian/ bullseye-backports main contrib non-free
-deb https://mirrors.tencentyun.com/debian-security bullseye-security main contrib non-free
-# deb-src https://mirrors.tencentyun.com/debian-security bullseye-security main contrib non-free
+deb http://mirrors.tencentyun.com/debian/ bullseye main contrib non-free
+# deb-src http://mirrors.tencentyun.com/debian/ bullseye main contrib non-free
+deb http://mirrors.tencentyun.com/debian/ bullseye-updates main contrib non-free
+# deb-src http://mirrors.tencentyun.com/debian/ bullseye-updates main contrib non-free
+deb http://mirrors.tencentyun.com/debian/ bullseye-backports main contrib non-free
+# deb-src http://mirrors.tencentyun.com/debian/ bullseye-backports main contrib non-free
+deb http://mirrors.tencentyun.com/debian-security bullseye-security main contrib non-free
+# deb-src http://mirrors.tencentyun.com/debian-security bullseye-security main contrib non-free
 EOF
 	;;
 12)
 	cat <<EOF >/etc/apt/sources.list
-deb https://mirrors.tencentyun.com/debian/ bookworm main contrib non-free non-free-firmware
-# deb-src https://mirrors.tencentyun.com/debian/ bookworm main contrib non-free non-free-firmware
-deb https://mirrors.tencentyun.com/debian/ bookworm-updates main contrib non-free non-free-firmware
-# deb-src https://mirrors.tencentyun.com/debian/ bookworm-updates main contrib non-free non-free-firmware
-deb https://mirrors.tencentyun.com/debian/ bookworm-backports main contrib non-free non-free-firmware
-# deb-src https://mirrors.tencentyun.com/debian/ bookworm-backports main contrib non-free non-free-firmware
-deb https://mirrors.tencentyun.com/debian-security bookworm-security main contrib non-free non-free-firmware
-# deb-src https://mirrors.tencentyun.com/debian-security bookworm-security main contrib non-free non-free-firmware
+deb http://mirrors.tencentyun.com/debian/ bookworm main contrib non-free non-free-firmware
+# deb-src http://mirrors.tencentyun.com/debian/ bookworm main contrib non-free non-free-firmware
+deb http://mirrors.tencentyun.com/debian/ bookworm-updates main contrib non-free non-free-firmware
+# deb-src http://mirrors.tencentyun.com/debian/ bookworm-updates main contrib non-free non-free-firmware
+deb http://mirrors.tencentyun.com/debian/ bookworm-backports main contrib non-free non-free-firmware
+# deb-src http://mirrors.tencentyun.com/debian/ bookworm-backports main contrib non-free non-free-firmware
+deb http://mirrors.tencentyun.com/debian-security bookworm-security main contrib non-free non-free-firmware
+# deb-src http://mirrors.tencentyun.com/debian-security bookworm-security main contrib non-free non-free-firmware
 EOF
 	;;
 *)
