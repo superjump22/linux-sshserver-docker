af05255ff09ca8d11c7089c8d22c9a617b3f2c26
Super Jump <superjump.void@gmail.com>
2024-04-18 13:19:44 +0800

    test

--- dockerfiles/centos/stream8/start-ssh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -z "$SSH_PWD" ]; then
-    echo "SSH_PWD not set"
-    exit 1
-fi
-sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/" /etc/ssh/sshd_config
-echo "root:$SSH_PWD" | chpasswd
-ssh-keygen -A
-/usr/sbin/sshd -D
--- dockerfiles/start-ssh.sh
+++ dockerfiles/start-ssh.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-if [ -z "$SSH_PWD" ]; then
-    echo "SSH_PWD not set"
+if [ -z "$ROOT_PASSWORD" ]; then
+    echo "ROOT_PASSWORD not set"
     exit 1
 fi
-if [ -z "$SSH_PORT" ]; then
-    echo "SSH_PORT not set"
-    exit 1
-fi
-sed -i "s/#Port 22/Port $SSH_PORT/" /etc/ssh/sshd_config
 sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/" /etc/ssh/sshd_config
-echo "root:$SSH_PWD" | chpasswd
+echo "root:$ROOT_PASSWORD" | chpasswd
 ssh-keygen -A
 /usr/sbin/sshd -D
